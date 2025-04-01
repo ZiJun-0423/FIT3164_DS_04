6391dd5bc6aa9c0634073340df5edbe0b2d803fa
ZiJun-0423 <weezijun@gmail.com>
2025-04-02 01:55:34 +1100

    updated database base access

--- backend/database/db_base.py
+++ backend/database/db_base.py
@@ -20,43 +20,4 @@ def get_db_connection():
     except Error as e:
         print(f"Error: {e}")
         return None
-    
-
-def get_games():
-    pass
-
-def get_game_by_id(game_id):
-    pass
-
-def add_game(game_data):
-    pass
-
-def update_game(game_id, game_data):
-    pass
-
-def delete_game(game_id):
-    pass
-
-def add_stat(stat_data):
-    pass
-
-def update_stat(stat_id, stat_data):
-    pass
-
-def delete_stat(stat_id):
-    pass
-
-def get_stats_by_game(game_id):
-    pass
-
-def get_stats_by_team(team_id):
-    pass
-
-def get_stats_by_game_and_team(game_id, team_id):
-    pass
-
-def get_elo_ratings():
-    pass
-
-def get_elo_rating_by_team(team_id):
-    pass
+        
