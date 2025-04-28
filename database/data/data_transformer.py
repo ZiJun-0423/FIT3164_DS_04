Fixed point calculation, and drawn matches for database

database/data/data_transformer.py
@@ -93,11 +93,22 @@ def standardise_date(date_str):
 df_merged['date'] = df_merged['date'].astype(str).apply(standardise_date)
 
 #generating team scores 
+def calculate_team_score(row, team_prefix):
+    # Check if ET goals and ET behinds are not missing
+    if row[f'{team_prefix}_et_goals'] > 0 and row[f'{team_prefix}_et_behinds'] > 0:
+        total_goals = row[f'{team_prefix}_et_goals']
+        total_behinds = row[f'{team_prefix}_et_behinds']
+    else:
+        total_goals = row[f'{team_prefix}_q4_goals']
+        total_behinds = row[f'{team_prefix}_q4_behinds']
+    
+    return 6 * total_goals + total_behinds
+
 df_merged[['team1_et_goals', 'team1_et_behinds', 'team2_et_goals', 'team2_et_behinds']] = df_merged[['team1_et_goals', 'team1_et_behinds', 'team2_et_goals', 'team2_et_behinds']].fillna(0).astype(int)
-df_merged['team1_score'] = 6 * (df_merged['team1_q1_goals'] + df_merged['team1_q2_goals'] + df_merged['team1_q3_goals'] + df_merged['team1_q4_goals'] + df_merged['team1_et_goals']) + df_merged['team1_q1_behinds'] + df_merged['team1_q2_behinds'] +df_merged['team1_q3_behinds'] + df_merged['team1_q4_behinds'] + df_merged['team1_et_behinds']
-df_merged['team2_score'] = 6 * (df_merged['team2_q1_goals'] + df_merged['team2_q2_goals'] + df_merged['team2_q3_goals'] + df_merged['team2_q4_goals'] + df_merged['team2_et_goals']) + df_merged['team2_q1_behinds'] + df_merged['team2_q2_behinds'] +df_merged['team2_q3_behinds'] + df_merged['team2_q4_behinds'] + df_merged['team2_et_behinds']
+df_merged['team1_score'] = df_merged.apply(lambda row: calculate_team_score(row, 'team1'), axis=1)
+df_merged['team2_score'] = df_merged.apply(lambda row: calculate_team_score(row, 'team2'), axis=1)
 
 #generating winner column
-df_merged['winner'] = np.where(df_merged['team1_score'] > df_merged['team2_score'], df_merged['team1'], df_merged['team2'])
+# df_merged['winner'] = np.where(df_merged['team1_score'] > df_merged['team2_score'], df_merged['team1'], df_merged['team2'])
 df_merged.to_csv("database/data/merged_data.csv")
 print("data cleaned and merged!")