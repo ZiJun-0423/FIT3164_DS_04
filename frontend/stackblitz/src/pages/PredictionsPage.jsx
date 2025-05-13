a69bc237dee505057a7fe0204ec5fd2ee7939c6d
eddent2009 <eteo0016@student.monash.edu>
2025-05-13 13:42:43 +1000

    correct teamlogo file path

--- frontend/stackblitz/src/pages/PredictionsPage.jsx
+++ frontend/stackblitz/src/pages/PredictionsPage.jsx
@@ -8,25 +8,25 @@ export default function PredictionsPage() {
   const [selectedTeamSlot, setSelectedTeamSlot] = useState('');
 
   const teams = [
-    { id: 1, name: 'Collingwood', logo: '/frontend/stackblitz/teamLogo/collingwood.png' },
-    { id: 2, name: 'Richmond', logo: '/frontend/stackblitz/teamLogo/richmondTigers.png' },
-    { id: 3, name: 'Geelong', logo: '/frontend/stackblitz/teamLogo/geelongCats.png' },
-    { id: 4, name: 'Brisbane Lions', logo: '/frontend/stackblitz/teamLogo/brisbaneLions.png' },
-    { id: 5, name: 'Adelaide', logo: '/frontend/stackblitz/teamLogo/adelaideCrows.png' },
-    { id: 6, name: 'West Coast Eagles', logo: '/frontend/stackblitz/teamLogo/westCoastEagles.png' },
-    { id: 7, name: 'Carlton', logo: '/frontend/stackblitz/teamLogo/carlton.png' },
-    { id: 8, name: 'Fremantle', logo: '/frontend/stackblitz/teamLogo/fremantle.png' },
-    { id: 9, name: 'Essendon', logo: '/frontend/stackblitz/teamLogo/essendon.png' },
-    { id: 10, name: 'Hawthorn Hawks', logo: '/frontend/stackblitz/teamLogo/hawthornHawks.png' },
-    { id: 11, name: 'Gold Coast Suns', logo: '/frontend/stackblitz/teamLogo/goldCoastSuns.png' },
-    { id: 12, name: 'Greater Western Sydney Giants', logo: '/frontend/stackblitz/teamLogo/greatWesternSydneyGiants.png' },
-    { id: 13, name: 'Melbourne', logo: '/frontend/stackblitz/teamLogo/melbourne.png' },
-    { id: 14, name: 'North Melbourne', logo: '/frontend/stackblitz/teamLogo/northMelbourne.png' },
-    { id: 15, name: 'Port Adelaide', logo: '/frontend/stackblitz/teamLogo/portAdelaide.png' },
-    { id: 16, name: 'St Kilda', logo: '/frontend/stackblitz/teamLogo/stKilda.png' },
-    { id: 17, name: 'Sydney Swans', logo: '/frontend/stackblitz/teamLogo/sydneySwan.png' },
-    { id: 18, name: 'Western Bulldogs', logo: '/frontend/stackblitz/teamLogo/westernBulldogs.jpg' },
-  ];
+    { id: 1, name: 'Collingwood', logo: '/teamlogo/collingwood.png' },
+    { id: 2, name: 'Richmond', logo: '/teamlogo/richmond.png' },
+    { id: 3, name: 'Geelong', logo: '/teamlogo/geelong.png' },
+    { id: 4, name: 'Brisbane Lions', logo: '/teamlogo/brisbanelions.png' },
+    { id: 5, name: 'Adelaide', logo: '/teamlogo/adelaide.png' },
+    { id: 6, name: 'West Coast Eagles', logo: '/teamlogo/westcoast.png' },
+    { id: 7, name: 'Carlton', logo: '/teamlogo/carlton.png' },
+    { id: 8, name: 'Fremantle', logo: '/teamlogo/fremantle.png' },
+    { id: 9, name: 'Essendon', logo: '/teamlogo/essendon.png' },
+    { id: 10, name: 'Hawthorn Hawks', logo: '/teamlogo/hawthorn.png' },
+    { id: 11, name: 'Gold Coast Suns', logo: '/teamlogo/goldcoast.png' },
+    { id: 12, name: 'Greater Western Sydney Giants', logo: '/teamlogo/greatwesternsydney.png' },
+    { id: 13, name: 'Melbourne', logo: '/teamlogo/melbourne.png' },
+    { id: 14, name: 'North Melbourne', logo: '/teamlogo/northmelbourne.png' },
+    { id: 15, name: 'Port Adelaide', logo: '/teamlogo/portadelaide.png' },
+    { id: 16, name: 'St Kilda', logo: '/teamlogo/stkilda.png' },
+    { id: 17, name: 'Sydney Swans', logo: '/teamlogo/sydney.png' },
+    { id: 18, name: 'Western Bulldogs', logo: '/teamlogo/westernbulldogs.png' }
+    ];
 
   const openModal = (teamSlot) => {
     setSelectedTeamSlot(teamSlot);
